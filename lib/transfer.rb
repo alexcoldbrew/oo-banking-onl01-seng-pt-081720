2c799faa7947cae7bcd2a24d71cff9ee79187831
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-22 01:01:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,7 +22,7 @@ class Transfer
       @status = 'rejected'
       return "Transaction rejected. Please check your account balance."
 
-    else
+    elsif status != 'complete'
       sender.balance -= amount
       receiver.balance += amount
       @status = 'complete'
