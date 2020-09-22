8a4544acd4d05e72305371ea565ba495f545bf29
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-22 00:57:51 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,7 +20,8 @@ class Transfer
   def execute_transaction
     if !sender.valid?
       "Transaction rejected. Please check your account balance."
-    elsif 
+    elsif @status == "complete"
+      "Transaction was already excuted"
       
     else
       sender.balance -= amount
