aaa9950fc89c0f3c6614799fb8bdf47dab871237
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-09-22 01:04:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,16 +18,16 @@ class Transfer
   end
   
   def execute_transaction
-    if valid? && sender.balance < amount
-      @status = 'rejected'
-      return "Transaction rejected. Please check your account balance."
-
-    elsif status != 'complete'
-      sender.balance -= amount
-      receiver.balance += amount
-      @status = 'complete'
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.withdrawal(self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
     end
   end
+
   
   def reverse_transfer
     
